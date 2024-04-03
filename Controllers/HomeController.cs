7a1d3b5712a6a7671baf27d6474f7e227d4dbd24
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2024-04-03 18:36:41 +0300

    ViewBag || ViewData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace MeetingApllicationProject.Controllers
 {
     public class HomeController : Controller
     {
-        //localhost/home url'si ile karsimiza gelecek.
+        // localhost/home URL'si ile karsimiza gelecek.
         public IActionResult Index()
         {
+            int saat = DateTime.Now.Hour;
+            //string selamlama = saat > 12 ? "İyi Günler" : "Günaydin";
+            // string userName = "Mucahit";
+            // ViewBag.Selamlama = selamlama;
+            // ViewBag.UserName = userName;
+
+            ViewBag.Selamlama = saat > 12 ? "İyi Günler" : "Günaydin";
+            ViewBag.UserName = "Mucahit";
             return View();
         }
     }
